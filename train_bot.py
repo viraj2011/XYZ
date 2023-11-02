4bd437630c373310d1a36be74be43674367b7863
viraj2011 <107635321+viraj2011@users.noreply.github.com>
2023-11-02 19:47:07 +0530

    Add files via upload

---
+++ train_bot.py
@@ -0,0 +1,15 @@
+#Text Data Preprocessing Lib
+
+
+# function for appending stem words
+
+
+
+    
+        # Add all words of patterns to list
+        
+        # Add all tags to the classes list
+         
+
+#Create word corpus for chatbot
+
